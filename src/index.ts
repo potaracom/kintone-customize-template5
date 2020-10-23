1382e619691d96284d52fc689726c07972b14f0b
potaracom <potara.com@gmail.com>
2020-10-23 16:58:01 +0900

    即時関数とStrictモードを削除

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,26 @@
-(() => {
-  "use strict";
+interface IndexEvent {
+  appId: number;
+  viewType: string;
+  viewId: number;
+  viewName: string;
+  records: kintone.types.SavedFields[];
+  offset: number;
+  size: number;
+  date: null;
+  type: string;
+}
 
-  interface IndexEvent {
-    appId: number;
-    viewType: string;
-    viewId: number;
-    viewName: string;
-    records: kintone.types.SavedFields[];
-    offset: number;
-    size: number;
-    date: null;
-    type: string;
-  }
+interface DetailEvent {
+  appId: number;
+  record: kintone.types.SavedFields;
+  recordId: number;
+  type: string;
+}
 
-  interface DetailEvent {
-    appId: number;
-    record: kintone.types.SavedFields;
-    recordId: number;
-    type: string;
-  }
+kintone.events.on("app.record.index.show", (event: IndexEvent) => {
+  console.log(Object.values(event));
+});
 
-  kintone.events.on("app.record.index.show", (event: IndexEvent) => {
-    console.log(Object.values(event));
-  });
-
-  kintone.events.on("app.record.detail.show", (event: DetailEvent) => {
-    console.log(Object.values(event));
-  });
-})();
+kintone.events.on("app.record.detail.show", (event: DetailEvent) => {
+  console.log(Object.values(event));
+});
